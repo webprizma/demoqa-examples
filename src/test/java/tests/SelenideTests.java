109fd593bfbe2609cf1fef5ac9f1cdcd5b146f49
Yuri Kulagin <kulagin1987@gmail.com>
2022-06-12 13:55:21 +0300

    little fixes

--- src/test/java/tests/SelenideTests.java
+++ src/test/java/tests/SelenideTests.java
@@ -3,16 +3,16 @@ package tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SelenideTests {
 
-    SelenideElement SoftAssertionsPage = $("div#wiki-pages-box").$$("a").findBy(Condition.text("SoftAssertions"));
+    SelenideElement softAssertionsPage = $("div#wiki-pages-box").$$("a").findBy(text("SoftAssertions"));
 
     @Test
     public void softAssertionsCheck() {
@@ -22,10 +22,10 @@ public class SelenideTests {
         $("a#wiki-tab").click();
 //      - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("li.wiki-more-pages-link").$("button").scrollTo().click();
-        SoftAssertionsPage.should(Condition.visible);
+        softAssertionsPage.should(Condition.visible);
 //      - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        SoftAssertionsPage.click();
-        $$("h4").findBy(Condition.text("JUnit5")).sibling(0).innerHtml().contains("<pre>");
+        softAssertionsPage.click();
+        $$("h4").findBy(text("JUnit5")).sibling(0).innerHtml().contains("<pre>");
     }
 
     SelenideElement columnA = $("#column-a");
@@ -38,7 +38,9 @@ public class SelenideTests {
 //        - Перенесите прямоугольник А на место В
         columnA.dragAndDropTo(columnB);
 //        - Проверьте, что прямоугольники действительно поменялись
-        $("#columns").$$(".column").first().should(Condition.text("B"));
+//        $("#columns").$$(".column").first().should(text("B"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 
     @DisplayName("Тест не работает с actions()")
@@ -49,6 +51,8 @@ public class SelenideTests {
 //        - Перенесите прямоугольник А на место В
         Selenide.actions().moveToElement(columnA).clickAndHold().moveByOffset(300, 200).release().perform();
 //        - Проверьте, что прямоугольники действительно поменялись
-        $("#columns").$$(".column").first().should(Condition.text("B"));
+//        $("#columns").$$(".column").first().should(text("B"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
