e503a543c240cbe487143b8f12b42d3b42012a60
Yuri Kulagin <kulagin1987@gmail.com>
2022-06-19 20:05:52 +0300

    refactoring

--- src/test/java/tests/Car.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class Car {
-
-    private String color;
-    private String type;
-
-    // standard getters setters
-}
--- src/test/java/tests/FileParseTest.java
+++ src/test/java/tests/FileParseTest.java
@@ -2,22 +2,23 @@ package tests;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.opencsv.CSVReader;
-import io.qameta.allure.selenide.AllureSelenide;
+import com.opencsv.exceptions.CsvException;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tests.domain.Person;
 
 import java.io.*;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -31,7 +32,7 @@ public class FileParseTest {
 
     @Disabled
     @Test
-    void pdfTest() throws Exception {
+    void pdfTest() throws IOException {
         Selenide.open("https://junit.org/junit5/docs/current/user-guide/");
         File pdf = $("a[href*='junit-user-guide-5.8.2.pdf']").download();
         PDF pdfParsed = new PDF(pdf);
@@ -40,7 +41,7 @@ public class FileParseTest {
 
     @Disabled
     @Test
-    void xlsTest() throws Exception {
+    void xlsTest() throws FileNotFoundException {
         Selenide.open("http://romashka2008.ru/price");
         File xls = $x("//h1[text()='Скачать Прайс-лист Excel']").sibling(0).$("a").download();
         XLS xlsParsed = new XLS(xls);
@@ -54,112 +55,102 @@ public class FileParseTest {
 
     @Disabled
     @Test
-    void csvTest() throws Exception {
-        InputStream is = classLoader.getResourceAsStream("1.csv");
-        CSVReader csvReader = new CSVReader(new InputStreamReader(is, UTF_8));
-        List<String[]> csv = csvReader.readAll();
-        assertThat(csv).contains(
-                new String[]{"sdhgdfhjdre", "dshghgdshg", "testData"}
-        );
+    void csvTest() throws IOException, CsvException {
+        try (InputStream is = classLoader.getResourceAsStream("1.csv")) {
+            CSVReader csvReader = new CSVReader(new InputStreamReader(is, UTF_8));
+            List<String[]> csv = csvReader.readAll();
+            assertThat(csv).contains(
+                    new String[]{"sdhgdfhjdre", "dshghgdshg", "testData"}
+            );
+        }
     }
 
     @Disabled
     @Test
-    void zipTest() throws Exception {
-        InputStream is = classLoader.getResourceAsStream("1.zip");
-        ZipInputStream zis = new ZipInputStream(is);
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-            String fileName = entry.getName();
-            assertThat(fileName).isEqualTo("1.txt");
-            //достаем файл из архива
-            FileOutputStream fout = new FileOutputStream("src/test/resources/" + entry.getName());
-            for (int c = zis.read(); c != -1; c = zis.read()) {
-                fout.write(c);
+    void zipTest() {
+        try (InputStream is = classLoader.getResourceAsStream("1.zip")) {
+            ZipInputStream zis = new ZipInputStream(is);
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                String fileName = entry.getName();
+                assertThat(fileName).isEqualTo("1.txt");
             }
-            fout.flush();
-            zis.closeEntry();
-            fout.close();
-            //проверяем содержимое файла
-            FileInputStream fileInZIP = new FileInputStream("src/test/resources/" + entry.getName());
-            byte[] fileContent = fileInZIP.readAllBytes();
-            assertThat(new String(fileContent, UTF_8)).contains("привет");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Disabled
     @Test
     void jsonTest() {
-        InputStream is = classLoader.getResourceAsStream("1.json");
-        Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(new InputStreamReader(is), JsonObject.class);
-        assertThat(jsonObject.get("name").getAsString()).isEqualTo("Yuri");
-        assertThat(jsonObject.get("goodBoy").getAsBoolean()).isEqualTo(true);
-        assertThat(jsonObject.get("passport").getAsJsonObject().get("number").getAsInt()).isEqualTo(1234);
+        try (InputStream is = classLoader.getResourceAsStream("1.json")) {
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(new InputStreamReader(is), JsonObject.class);
+            assertThat(jsonObject.get("name").getAsString()).isEqualTo("Yuri");
+            assertThat(jsonObject.get("goodBoy").getAsBoolean()).isEqualTo(true);
+            assertThat(jsonObject.get("passport").getAsJsonObject().get("number").getAsInt()).isEqualTo(1234);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Disabled
     @Test
     void jsonTestAlt() {
-        InputStream is = classLoader.getResourceAsStream("1.json");
-        Gson gson = new Gson();
-        Person jsonObject = gson.fromJson(new InputStreamReader(is), Person.class);
-        assertThat(jsonObject.getName()).isEqualTo("Yuri");
-        assertThat(jsonObject.getGoodBoy()).isEqualTo(true);
-        assertThat(jsonObject.getPassport().getNumber()).isEqualTo(1234);
+        try (InputStream is = classLoader.getResourceAsStream("1.json")) {
+            Gson gson = new Gson();
+            Person jsonObject = gson.fromJson(new InputStreamReader(is), Person.class);
+            assertThat(jsonObject.getName()).isEqualTo("Yuri");
+            assertThat(jsonObject.getGoodBoy()).isEqualTo(true);
+            assertThat(jsonObject.getPassport().getNumber()).isEqualTo(1234);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DisplayName("Реализовать чтение и проверку содержимого каждого файла из архива testArchive.zip")
     @Test
-    void testArchiveTest() throws Exception {
-        InputStream is = classLoader.getResourceAsStream("testArchive.zip");
-        ZipInputStream zis = new ZipInputStream(is);
-        ZipEntry entry;
-        while ((entry = zis.getNextEntry()) != null) {
-            String fileName = entry.getName();
-            FileOutputStream fout = new FileOutputStream("build/resources/test/testArchive/" + fileName);
-            for (int c = zis.read(); c != -1; c = zis.read()) {
-                fout.write(c);
-            }
-            fout.flush();
-            zis.closeEntry();
-            fout.close();
-
-            FileInputStream fileInZIP = new FileInputStream("build/resources/test/testArchive/" + fileName);
-            if (fileName.contains("csv")) {
-                CSVReader csvReader = new CSVReader(new InputStreamReader(fileInZIP, UTF_8));
-                List<String[]> csv = csvReader.readAll();
-                assertThat(csv).contains(new String[]{"johnson81;4081;Craig;Johnson"}
-                );
-            } else if (fileName.contains(("pdf"))) {
-                PDF pdfParsed = new PDF(fileInZIP);
-                System.out.println(pdfParsed.creator);
-                assertThat(pdfParsed.creator).isEqualTo("Rave (http://www.nevrona.com/rave)");
-            } else if (fileName.contains(("xls"))) {
-                XLS xlsParsed = new XLS(fileInZIP);
-                assertThat(
-                        xlsParsed.excel.getSheetAt(0)
-                                .getRow(5)
-                                .getCell(2)
-                                .getStringCellValue()
-                ).contains("Ashish");
-            } else {
-                System.out.println("Расширение файла не распознано");
+    void testArchiveTest() throws IOException, CsvException {
+        try (ZipFile zipFile = new ZipFile("src/test/resources/testArchive.zip")) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                InputStream stream = zipFile.getInputStream(entry);
+                if (entry.getName().contains("csv")) {
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(stream, UTF_8));
+                    List<String[]> csv = csvReader.readAll();
+                    assertThat(csv).contains(new String[]{"johnson81;4081;Craig;Johnson"});
+                    System.out.println("Информация в CSV файле найдена");
+                } else if (entry.getName().contains(("pdf"))) {
+                    PDF pdfParsed = new PDF(stream);
+                    System.out.println(pdfParsed.creator);
+                    assertThat(pdfParsed.creator).isEqualTo("Rave (http://www.nevrona.com/rave)");
+                    System.out.println("Информация в PDF файле найдена");
+                } else if (entry.getName().contains(("xls"))) {
+                    XLS xlsParsed = new XLS(stream);
+                    assertThat(
+                            xlsParsed.excel.getSheetAt(0)
+                                    .getRow(5)
+                                    .getCell(2)
+                                    .getStringCellValue()
+                    ).contains("Ashish");
+                    System.out.println("Информация в XLS файле найдена");
+                } else {
+                    System.out.println("Расширение файла не распознано");
+                }
             }
         }
     }
 
     @DisplayName("Реализовать разбор json файла библиотекой Jackson")
     @Test
-    void jsonJackson() throws Exception {
+    void jsonJackson() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        InputStream is = classLoader.getResourceAsStream("myExample.json");
-        JsonNode jsonNode = objectMapper.readTree(new InputStreamReader(is, UTF_8));
-
-        assertThat(jsonNode.get("Name").asText()).isEqualTo("Craig");
-        assertThat(jsonNode.withArray("BookInterests").findValue("Book").asText()).isEqualTo("The Kite Runner");
-        assertThat((jsonNode.findValue("FoodInterests")).withArray("Breakfast").findValue("Bread").asText()).isEqualTo("Whole wheat");
-
+        try (InputStream is = classLoader.getResourceAsStream("myExample.json")) {
+            JsonNode jsonNode = objectMapper.readTree(new InputStreamReader(is, UTF_8));
+            assertThat(jsonNode.get("Name").asText()).isEqualTo("Craig");
+            assertThat(jsonNode.withArray("BookInterests").findValue("Book").asText()).isEqualTo("The Kite Runner");
+            assertThat((jsonNode.findValue("FoodInterests")).withArray("Breakfast").findValue("Bread").asText()).isEqualTo("Whole wheat");
+        }
     }
-
 }
