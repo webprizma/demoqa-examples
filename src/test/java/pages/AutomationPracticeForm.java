1a9a87939ece9500610c15c693005663b1012d74
Yuri Kulagin <kulagin1987@gmail.com>
2022-06-22 12:33:02 +0300

    added jenkins annotations, attachments and selenoid config

--- src/test/java/pages/AutomationPracticeForm.java
+++ src/test/java/pages/AutomationPracticeForm.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.ResultTableComponent;
 
@@ -26,6 +27,7 @@ public class AutomationPracticeForm {
             genderWrapper = $("#genterWrapper"),
             submitButton = $("#submit");
 
+    @Step("Открываем страницу https://demoqa.com/automation-practice-form")
     public AutomationPracticeForm openPage() {
         open("/automation-practice-form");
         practiceFormWrapper.shouldHave(text("Student Registration Form"));
@@ -34,111 +36,111 @@ public class AutomationPracticeForm {
 
         return this;
     }
-
+    @Step("Вводим имя")
     public AutomationPracticeForm setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
-
+    @Step("Вводим фамилию")
     public AutomationPracticeForm setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
-
+    @Step("Вводим имя и фамилию")
     public AutomationPracticeForm setFullName(String firstName, String lastName) {
         firstNameInput.setValue(firstName);
         lastNameInput.setValue(lastName);
 
         return this;
     }
-
+    @Step("Вводим email")
     public AutomationPracticeForm setEmail(String value) {
         emailInput.setValue(value);
 
         return this;
     }
-
+    @Step("Выбираем пол")
     public AutomationPracticeForm setGender(String value) {
         genderWrapper.$(byText(value)).click();
         //genderWrapper.$(byText(value)).parent().click();
 
         return this;
     }
-
+    @Step("Вводим номер телефона")
     public AutomationPracticeForm setPhoneNumber(String value) {
         phoneNumberInput.setValue(value);
 
         return this;
     }
-
+    @Step("Указываем дату рождения")
     public AutomationPracticeForm setDateOfBirth(String month, String year, String day) {
         dateOfBirthField.click();
         calendarComponent.setDate(month, year, day);
 
         return this;
     }
-
+    @Step("Указываем дату рождения вводом значения")
     public AutomationPracticeForm setDateOfBirthWithKeys(String month, String year, String day) {
         dateOfBirthField.clear();
         dateOfBirthField.sendKeys(day + " " + month + " " + year);
 
         return this;
     }
-
+    @Step("Выбираем предметы")
     public AutomationPracticeForm setSubjects(String value) {
         subjectsInput.sendKeys(value);
         subjectsInput.pressEnter();
 
         return this;
     }
-
+    @Step("Выбираем хобби")
     public AutomationPracticeForm setHobby(String value) {
         hobbiesWrapper.$(byText(value)).click();
 
         return this;
     }
-
+    @Step("Загружаем файл")
     public AutomationPracticeForm uploadFile(String value) {
         //uploadPictureButton.uploadFile(value);
         uploadPictureButton.uploadFromClasspath(value);
 
         return this;
     }
-
+    @Step("Вводим адрес")
     public AutomationPracticeForm setAddress(String value) {
         currentAddressInput.setValue(value);
 
         return this;
     }
-
+    @Step("Выбираем штат")
     public AutomationPracticeForm setState(String value) {
         stateDropDown.scrollTo().click();
         stateDropDown.$(byText(value)).click();
 
         return this;
     }
-
+    @Step("Выбираем город")
     public AutomationPracticeForm setCity(String value) {
         cityDropDown.click();
         cityDropDown.$(byText(value)).click();
 
         return this;
     }
-
+    @Step("Отправляем форму")
     public AutomationPracticeForm sendForm() {
         submitButton.scrollTo().click();
 
         return this;
     }
-
+    @Step("Проверяем, что форма открылась")
     public AutomationPracticeForm checkFormOpened() {
         resultTableComponent.checkResultTableHeader();
 
         return this;
     }
-
+    @Step("Проверяем результаты")
     public AutomationPracticeForm checkResult(String key, String value) {
         resultTableComponent.checkResult(key, value);
 
--- src/test/java/tests/FormTests.java
+++ src/test/java/tests/FormTests.java
@@ -27,6 +27,7 @@
 package tests;
 
 import enums.Gender;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -34,23 +35,24 @@ import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
+@Owner("kulaginyv")
+@Feature("Form filling tests")
 public class FormTests extends TestBase {
-
+    @Test
     @DisplayName("Заполнение ВСЕХ полей и проверка наличия ВСЕХ введенных значений в модальном окне")
     //@Disabled("Not needed right now")
-    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Story("Заполнение ВСЕХ полей и проверка наличия ВСЕХ введенных значений в модальном окне")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void fullTest() {
-        automationPracticeForm
-                .setFullName(testData.firstName, testData.lastName)
-                //.setFirstName(firstName)
-                //.setLastName(lastName)
+        automationPracticeForm.setFullName(testData.firstName, testData.lastName)
                 .setEmail(testData.email)
                 .setGender(testData.gender)
                 .setPhoneNumber(testData.phone)
                 .setDateOfBirth(testData.birthMonth,
                         testData.birthYear,
                         testData.birthDay)
-                //.setDateOfBirthWithKeys(birthMonth, birthYear, birthDay)
                 .setSubjects(testData.subject)
                 .setHobby(testData.hobby)
                 .uploadFile(testData.fileName)
@@ -72,9 +74,13 @@ public class FormTests extends TestBase {
                 .checkResult("State and City", testData.state + " " + testData.city);
     }
 
+    @Test
     @DisplayName("Заполнение только необходимых для отправки формы полей и проверка, что эти данные попали в результирующую форму")
     //@Disabled("Not needed right now")
-    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Заполнение только необходимых для отправки формы полей и проверка, что эти данные попали в результирующую форму")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void onlyRequiredFieldsTest() {
         automationPracticeForm
                 .setFullName(testData.firstName, testData.lastName)
@@ -88,13 +94,17 @@ public class FormTests extends TestBase {
                 .checkResult("Mobile", testData.phone);
     }
 
-    @DisplayName("Parameterized Test With Value Source")
     @ParameterizedTest(name = "При вводе номера телефона {0} в результатах (форме) должен быть номер телефона {0}")
     @ValueSource(strings = {
             "9991234567",
             "9091234567",
             "9631234567"
     })
+    @DisplayName("Parameterized Test With Value Source")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Parameterized Test With Value Source")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void parameterizedTestWithValueSource(String valueSource) {
         automationPracticeForm
                 .setFullName(testData.firstName, testData.lastName)
@@ -108,13 +118,17 @@ public class FormTests extends TestBase {
                 .checkResult("Mobile", valueSource);
     }
 
-    @DisplayName("Parameterized Test With CSV Source")
     @ParameterizedTest(name = "При вводе имени {0}, фамилии {1}, пола {2} и телефона {3} в результатах (форме) должны быть имя {0}, фамилия {1}, пол {2} и телефон {3}")
     @CsvSource(value = {
             "Ivan, Ivanov, Male, 9991234567",
             "Petr, Petrov, Other, 9993214567",
             "Oksana, Brilski, Female, 9991112233"
     })
+    @DisplayName("Parameterized Test With CSV Source")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Parameterized Test With CSV Source")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void parameterizedTestWithCSVSource(String firstName, String lastName, String gender, String phone) {
         automationPracticeForm
                 .setFullName(firstName, lastName)
@@ -128,9 +142,14 @@ public class FormTests extends TestBase {
                 .checkResult("Mobile", phone);
     }
 
-    @DisplayName("Parameterized Test With Enum Source")
+
     @ParameterizedTest(name = "При выборе пола {0} в результатах (форме) пол должен быть {0}")
     @EnumSource(Gender.class)
+    @DisplayName("Parameterized Test With Enum Source")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Parameterized Test With Enum Source")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void parameterizedTestWithEnumSource(Gender gender) {
         automationPracticeForm
                 .setFullName(testData.firstName, testData.lastName)
@@ -152,9 +171,14 @@ public class FormTests extends TestBase {
         );
     }
 
-    @DisplayName("Parameterized Test With Method Source")
+
     @ParameterizedTest(name = "При выборе пола {0} в результатах (форме) пол должен быть {1}")
     @MethodSource(value = "methodSource")
+    @DisplayName("Parameterized Test With Method Source")
+    @Severity(SeverityLevel.MINOR)
+    @Story("Parameterized Test With Method Source")
+    @Description("Функциональный тест")
+    @Link(name = "demoqa.com", url = "https://demoqa.com/automation-practice-form")
     public void parameterizedTestWithMethodSource(String searchData, String expectedResult) {
         automationPracticeForm
                 .setFullName(testData.firstName, testData.lastName)
